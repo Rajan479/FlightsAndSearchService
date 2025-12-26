44304b9d0bce42d1da2e833345ac7c107bbf74f2
Rajan Parmar <rajanparmar2522@gmail.com>
2025-12-26 15:59:17 +0530

    Setup the .env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import express from 'express'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import PORT from './config/serverConfig.js';
+
+dotenv.config();
+
+const setUpAndStartServer =  function(){
+    const app = express();
+    app.listen(PORT, function ServerStarted(){
+        console.log(`Server started on ${PORT}`);
+    });
+}
+
+setUpAndStartServer();
