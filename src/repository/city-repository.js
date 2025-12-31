d82a82fdf57ad353134b0f1342a8712b8d24f303
Rajan Parmar <rajanparmar2522@gmail.com>
2025-12-31 11:24:25 +0530

    Updated the city-repository and added the CRUD oprations

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,24 +3,52 @@ const { City } = require('../models/index.js');
 class CityRepository{
 
     async createCity({ name }){
-        try {
-            const city = await City.create({ name });
+        try{
+            const city = await City.create({
+                name : name
+            });
             return city;
-        } catch (error) {
-            throw { error };
+        }
+        catch(error){
+            console.log("Something went wrong in the repository layer" + error);
         }
     }
 
-    async deleteCity({ cityID }){
+    async updateCity(cityId,{ name }){
         try{
-            await City.destroy({
-                where: {
-                    id: cityID
+            const city = await City.update(name, {
+                where : {
+                    id : cityId
                 }
-            })
+            });
+            return city;
         }
         catch(error){
-            throw { error };
+            console.log("Something went wrong in the repository layer" + error);
+        }
+    }
+
+    async deleteCity(cityId){
+        try{
+            await City.destroy({
+                where : {
+                    id : cityId
+                }
+            });
+            return true;
+        }
+        catch(error){
+            console.log("Something went wrong in the repository layer" + error);
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = City.findByPk(cityId);
+            return city;
+        }
+        catch(error){
+            console.log("Something went wrong in the repository layer" + error);
         }
     }
 
