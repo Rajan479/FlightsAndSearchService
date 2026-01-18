cc599b268a7c463b71cd2966d82c05960d2a9d4b
Rajan Parmar <rajanparmar2522@gmail.com>
2026-01-18 11:45:47 +0530

    Updated the class based code to function based code

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,85 +1,85 @@
 const { City } = require('../models/index.js');
 const { Op } = require('sequelize');
 
-class CityRepository{
-
-    async createCity({ name }){
-        try{
-            const city = await City.create({
-                name : name
-            });
-            return city;
-        }
-        catch(error){
-            console.log("Something went wrong in the repository layer");
-            throw new {error};
-        }
+const createCity = async function create({ name }){
+    try{
+        const city = await City.create({
+            name : name
+        });
+        return city;
     }
-
-    async updateCity(cityId, updatedCity){
-        try{
-            const city = await City.update(updatedCity, {
-                where : {
-                    id : cityId
-                }
-            });
-            return city;
-        }
-        catch(error){
-            console.log("Something went wrong in the repository layer");
-            throw new {error};
-        }
+    catch(error){
+        console.log("Something went wrong in the repository layer");
+        throw new {error};
     }
+}
 
-    async deleteCity(cityId){
-        try{
-            await City.destroy({
-                where : {
-                    id : cityId
-                }
-            });
-            return true;
-        }
-        catch(error){
-            console.log("Something went wrong in the repository layer");
-            throw new {error};
-        }
-    }
-
-    async getCity(cityId){
-        try{
-            const city = await City.findByPk(cityId);
-            return city;
-        }
-        catch(error){
-            console.log("Something went wrong in the repository layer");
-            throw new {error};
-        }
-    }
-
-    async getAllCities(filter){
-        try{
-            if(filter.name){
-                const cities = await City.findAll({
-                    where : {
-                        name : {
-                            [Op.startsWith] : filter.name
-                        }
-                    }
-                });
-                return cities;
+const updateCity = async function update(cityId, updatedCity){
+    try{
+        const city = await City.update(updatedCity, {
+            where : {
+                id : cityId
             }
-            const cities = await City.findAll();
+        });
+        return city;
+    }
+    catch(error){
+        console.log("Something went wrong in the repository layer");
+        throw new {error};
+    }
+}
+
+const deleteCity = async function deletecity(cityId){
+    try{
+        await City.destroy({
+            where : {
+                id : cityId
+            }
+        });
+        return true;
+    }
+    catch(error){
+        console.log("Something went wrong in the repository layer");
+        throw new {error};
+    }
+}
+
+const getCity = async function get(cityId){
+    try{
+        const city = await City.findByPk(cityId);
+        return city;
+    }
+    catch(error){
+        console.log("Something went wrong in the repository layer");
+        throw new {error};
+    }
+}
+
+const getAllCities = async function getAll(filter){
+    try{
+        if(filter.name){
+            const cities = await City.findAll({
+                where : {
+                    name : {
+                        [Op.startsWith] : filter.name
+                    }
+                }
+            });
             return cities;
         }
-        catch(error){
-            console.log("Something went wrong in the repository layer");
-            throw new {error};
-        }
+        const cities = await City.findAll();
+        return cities;
     }
-
+    catch(error){
+        console.log("Something went wrong in the repository layer");
+        throw new {error};
+    }
 }
 
 module.exports = {
-    CityRepository : CityRepository
-}
+    createCity,
+    updateCity,
+    deleteCity,
+    getCity,
+    getAllCities
+}; 
