e197cb5d5300e165321ac1258c5fd45fdbba67a9
Rajan Parmar <rajanparmar2522@gmail.com>
2026-01-20 17:09:07 +0530

    Added some dummy data through seeders

--- src/seeders/20260108095748-add-airports.js
+++ src/seeders/20260108095748-add-airports.js
@@ -12,6 +12,37 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+    await queryInterface.bulkInsert('Airports', [
+      {
+        airportName : "Vadodara International Airport",
+        address : "Civil Aerodrome, Harni Rd, Harni, Vadodara, Gujarat 390022",
+        cityId : 1,
+        createdAt : new Date(),
+        updatedAt : new Date()
+      },
+      {
+        airportName : "Sardar Vallabhbhai Patel International Airport",
+        address : "Hansol, Ahmedabad, Gujarat 380003",
+        cityId : 6,
+        createdAt : new Date(),
+        updatedAt : new Date()
+      },
+      {
+        airportName : "Surat International Airport",
+        address : "Surat - Dumas Rd, Gaviyer, Surat, Gujarat 395007",
+        cityId : 4,
+        createdAt : new Date(),
+        updatedAt : new Date()
+      },
+      {
+        airportName : "Devi Ahilyabai Holkar International Airport",
+        address : "Indore, Madhya Pradesh 452005",
+        cityId : 5,
+        createdAt : new Date(),
+        updatedAt : new Date()
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
