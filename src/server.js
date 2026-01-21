7c22bc358257745b261247dd079aaf676c31a422
Rajan Parmar <rajanparmar2522@gmail.com>
2026-01-21 17:39:42 +0530

    Rename the file of index.js to server.js

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig.js');
+const ApiRoutes = require('./routes/index.js');
+const db = require('./models/index.js');
+
+const setUpAndStartServer =  function(){
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended:true }));
+
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, function ServerStarted(){
+        console.log(`Server started on ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({ alter: true });
+        }
+    });
+}
+
+setUpAndStartServer();
